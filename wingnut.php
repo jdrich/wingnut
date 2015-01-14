b8e114916485e8a5659df69211973a20c91c22d5
Jonathan Rich <jrich@richj-GHG2J02.ECS.local>
2015-01-14 16:45:24 -0500

    Using ConsoleKit for command line interaction, adding shell script.

--- wingnut.php
+++ wingnut.php
@@ -2,41 +2,6 @@
 
 require_once 'vendor/autoload.php';
 
-$wrapper = function($text, $render) {
-    return $render->render($text);
-};
+$command = Wingnut\CommandParser()->parse($argv);
 
-$context = [
-    'here' => 'World!',
-    'friends' => [
-        ['name' => 'Bobby'],
-        ['name' => 'Julie'],
-        ['name' => 'Frank']
-    ],
-    'karl' => $wrapper
-];
-
-$partial = <<<MUCHOPARTIAL
-    Tasdingoo, {{here}}
-
-{{#karl}}
-{{here}}
-{{/karl}}
-MUCHOPARTIAL;
-
-$text = <<<MUCHOGRANDE
-Hello _{{here}}_
-{{> troll.bagel}}
-
-{{#friends}}
-#{{name}}
-{{/friends}}
-MUCHOGRANDE;
-
-$parsedown = new Parsedown();
-$mustache = new Mustache_Engine();
-
-$loader = $mustache->getPartialsLoader();
-$loader->setTemplate('troll.bagel', $partial);
-
-echo $parsedown->text($mustache->loadTemplate($text)->render($context));
+var_dump($command);
